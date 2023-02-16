35510ea181ee7ab7735d6407df49d2edebe218da
Lexxx <simankovich@inbox.ru>
2023-02-16 18:21:08 +0400

    создан метод для формирования нового массива

---
+++ Decision/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] ReadyArray = {"hello", "2", "world", ":-)"};
+
+//Метод создания массива по условиям задачи
+string[] CreateNewArray(string[] ReadyArray)
+{
+    string[] NewArray = new string[ReadyArray.Length];
+    int count = 0;
+    for (int i = 0; i < ReadyArray.Length; i++)
+    {
+        if ( ReadyArray[i].Length <= 3)
+        {
+            NewArray[count] = ReadyArray[i];
+            count++;
+        }
+    } return NewArray;
+}
